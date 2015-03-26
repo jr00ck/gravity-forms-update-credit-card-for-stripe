0d95a1917c34e312f08229c5e69dfa65b638a26a
jr00ck <jeremy@webguysaz.com>
2015-03-25 17:04:51 -0700

    Initial plugin header setup
    Initial commit to create main plugin file with plugin header details

---
+++ gravity-forms-update-credit-card-for-stripe.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Gravity Forms Update Credit Card for Stripe
+Plugin URI: https://github.com/jr00ck/gravity-forms-update-credit-card-for-stripe
+Description: Use Gravity Forms to automatically notify your Stripe customers when their subscription payment fails and allow them to update their card on file to resume payments.
+Version: 1.0
+Author: FreeUp
+Author URI: http://freeupwebstudio.com
+Author Email: jeremy@freeupwebstudio.com
+Github Plugin URI: https://github.com/jr00ck/gravity-forms-update-credit-card-for-stripe
+*/
+
